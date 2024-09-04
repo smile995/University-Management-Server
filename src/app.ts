0c237df1d7c95961c7e84d1e4f0b1ef96e67f5ce
smile995 <ah.smile995@gmai.com>
2024-09-04 09:22:17 +0600

    student controller, routes and servises are ready

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from "express"
 import cors from "cors"
+import { StudentRoutes } from "./app/modules/student/student.route";
 
 const express = require('express')
 const app = express()
 // perser
 app.use(cors());
 app.use(express.json())
-// const route=app.router()
+
+// application routers
+app.use("/api/v1/students", StudentRoutes)
+
+
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,12 +1,12 @@
-// import express from 'express';
-// import { StudentControllers } from './student.controller';
+import express from 'express';
+import { StudentControllers } from './student.controller';
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post('/create-student', StudentControllers.createStudent);
+router.post('/create-student', StudentControllers.createStudent);
 
-// router.get('/', StudentControllers.getAllStudents);
+router.get('/', StudentControllers.getAllStudents);
 
-// router.get('/:studentId', StudentControllers.getSingleStudent);
+router.get('/:studentId', StudentControllers.getSingleStudent);
 
-// export const StudentRoutes = router;
+export const StudentRoutes = router;
