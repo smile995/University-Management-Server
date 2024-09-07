78f173676e0357bb681b95e8e39f2403615932bf
smile995 <ah.smile995@gmai.com>
2024-09-07 22:27:53 +0600

    user interface, model and validation schema is ready

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,24 +1,16 @@
-// export type TUser={
-//     id:string,
-//     password:string,
-//     needPasswordChange:boolean,
-//     role:"student" |"faculty"|"admin",
-//     status: "in-progress"|"blocked",
-//     isDeleted:boolean
-// }
-
 import { model, Schema } from "mongoose";
 import { TUser } from "./user.interface";
 
-const userSchema=new Schema<TUser>({
-    id:{ type:String, required:true},
-    password:{type:String,required:true},
-    needPasswordChange:{type:Boolean,default:true},
-    role:{ type:String,enum:['student','faculty','admin'],trim:true},
-    status:{ type:String,enum:['in-progress','blocked'],trim:true},
-   isDeleted:{type:Boolean,default:false}
+const userSchema = new Schema<TUser>({
+    id: { type: String, required: true },
+    password: { type: String, required: true },
+    needPasswordChange: { type: Boolean, default: true },
+    role: { type: String, enum: ['student', 'faculty', 'admin'], trim: true },
+    status: { type: String, enum: ['in-progress', 'blocked'], trim: true, default: 'in-progress' },
+    isDeleted: { type: Boolean, default: false }
 
-},{timestamps:true});
+}, { timestamps: true });
 
 // 3. Create a Model.
-const userModel = model<TUser>('User', userSchema);
+const UserModel = model<TUser>('User', userSchema);
+export default UserModel
